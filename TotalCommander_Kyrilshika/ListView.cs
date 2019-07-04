7ef335ce9839092b22906d792974bb6b16c01cb5
prokhor_kostja <prokhor.kostja@gmail.com>
2019-07-04 17:49:44 +0300

    commit

---
+++ TotalCommander_Kyrilshika/ListView.cs
@@ -0,0 +1,98 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TotalCommander_Kyrilshika
+{
+    class ListView
+    {
+        private int prevSelectedIndex;
+        private int selectedIndex;
+        private bool wasPainted;
+
+        private int scroll;
+        private int x, y, height;
+
+        public ListView(int x, int y, int height)
+        {
+            this.x = x;
+            this.y = y;
+            this.height = height;
+        }
+         public List<int> ColumnsWidth { get; set; }
+        public List<ListViewItem> Items { get; set; }
+
+        public void Clean()
+        {
+            selectedIndex = prevSelectedIndex = 0;
+            wasPainted = false;
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Console.CursorLeft = x;
+                Console.CursorTop = i + y;
+                Items[i].Clean(ColumnsWidth, i, x, y);
+            }
+        }
+        public ListViewItem SelectedItem => Items[selectedIndex];
+
+        public bool Focused { get; set; }
+
+        public void Render()
+        {
+            for (int i = 0; i < Math.Min(height, Items.Count); i++)
+            {
+                int elementIndex = i + scroll;
+
+                if (wasPainted)
+                {
+                    if (elementIndex != selectedIndex && elementIndex != prevSelectedIndex)
+                        continue;
+                }
+
+                var item = Items[elementIndex];
+                var savedForeground = Console.ForegroundColor;
+                var savedBackGround = Console.BackgroundColor;
+                if(elementIndex == selectedIndex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                }
+
+                Console.CursorLeft = x;
+                Console.CursorTop = i + y;
+                item.Render(ColumnsWidth, i, x, y);
+
+                Console.ForegroundColor = savedForeground;
+                Console.BackgroundColor = savedBackGround;
+            }
+            wasPainted = true;
+        }
+
+        public void Update(ConsoleKeyInfo key)
+        {
+            prevSelectedIndex = selectedIndex;
+
+            if (key.Key == ConsoleKey.DownArrow && selectedIndex + 1 < Items.Count)
+                selectedIndex++;
+            else if (key.Key == ConsoleKey.UpArrow && selectedIndex - 1 >= 0)
+                selectedIndex--;
+
+            if(selectedIndex >= height + scroll)
+            {
+                scroll++;
+                wasPainted = false;
+            }
+            else if (selectedIndex < scroll)
+            {
+                scroll--;
+                wasPainted = false;
+            }
+            else if (key.Key == ConsoleKey.Enter)
+                Selected(this, EventArgs.Empty);
+        }
+
+        public event EventHandler Selected;
+    }
+}
--- TotalCommander_Kyrilshika/Program.cs
+++ TotalCommander_Kyrilshika/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,44 @@ namespace TotalCommander_Kyrilshika
     {
         static void Main(string[] args)
         {
+            Console.CursorVisible = false;
+
+            var view = new ListView(10, 2, height: 20);
+            view.ColumnsWidth = new List<int> { 50, 10, 10 };
+
+            view.Items = GetItems("C:\\");
+            view.Selected += View_Selected;
+
+            while (true)
+            {
+                var key = Console.ReadKey();
+                view.Update(key);
+                view.Render();
+            }
+        }
+
+        private static List<ListViewItem> GetItems(string path)
+        {
+            return new DirectoryInfo(path).GetFileSystemInfos()
+                .Select(f => new ListViewItem(
+                    f,
+                    f.Name,
+                    f is DirectoryInfo dir ? "<dir>" : f.Extension,
+                    f is FileInfo file ? file.Length.ToString() : ""
+                    )).ToList();
+        }
+
+        private static void View_Selected(object sender, EventArgs e)
+        {
+            var view = (ListView)sender;
+            var info = view.SelectedItem.State;
+            if(info is FileInfo file)
+                Process.Start(file.FullName);
+            else if (info is DirectoryInfo dir)
+            {
+                view.Clean();
+                view.Items = GetItems(dir.FullName);
+            }
         }
     }
 }
